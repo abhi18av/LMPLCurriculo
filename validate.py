c0663341dfef07f8aaceb3cffe2e4cefa6f2f0b2
Abhinav Sharma <abhi18av@users.noreply.github.com>
2018-11-11 13:57:14 +0530

    Updated for python3

--- validate.py
+++ validate.py
@@ -1,12 +1,12 @@
 import sys
-from StringIO import StringIO
+from io import StringIO
 from lxml import etree
 
 try:
     dtd = etree.DTD(sys.argv[1])
     root = etree.parse(sys.argv[2])
-    if dtd.validate(root) == False:
-        print dtd.error_log.filter_from_errors()
+    if(dtd.validate(root) == False):
+        print(dtd.error_log.filter_from_errors())
 except:
-    print 'USE:\n\t validate.py <arquivo> \n'
+    print('USE:\n\t validate.py <arquivo> \n')
     sys.exit(1)
